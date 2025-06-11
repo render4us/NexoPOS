6321cff73ac9d6ae3700f0c5e57e0d4f4a4f0287
Vitor Martins Gonçalves <49039168+render4us@users.noreply.github.com>
2025-06-11 11:21:38 -0300

    feat(mercadopago): add payment type selector

--- modules/MercadoPago/Resources/ts/mercadopago-payment.vue
+++ modules/MercadoPago/Resources/ts/mercadopago-payment.vue
@@ -1,14 +1,31 @@
 <template>
-    <sample-payment :identifier="identifier" :label="label" @submit="processPayment"></sample-payment>
+    <div class="h-full w-full flex flex-col">
+        <div class="p-2">
+            <select v-model="paymentType" class="w-full border rounded p-2">
+                <option value="credit_card">Cartão de Crédito</option>
+                <option value="debit_card">Cartão de Débito</option>
+                <option value="pix">PIX</option>
+            </select>
+        </div>
+        <sample-payment
+            :identifier="identifier"
+            :label="label"
+            @submit="processPayment"/>
+    </div>
 </template>
 <script>
 import samplePayment from "~/pages/dashboard/pos/payments/sample-payment.vue";
-import { nsHttpClient } from "~/bootstrap";
+import { nsHttpClient, nsSnackBar } from "~/bootstrap";
 
 export default {
     name: 'mercadopago-payment',
     props: ['identifier', 'label'],
     components: { samplePayment },
+    data() {
+        return {
+            paymentType: 'credit_card',
+        };
+    },
     mounted() {
         console.log('MercadoPago payment component mounted');
     },
@@ -16,8 +33,8 @@ export default {
         async processPayment() {
             try {
                 const order = POS.order.getValue();
-                console.log('Sending order to /api/mercadopago/pay', order);
-                const response = await nsHttpClient.post('/api/mercadopago/pay', { order }).toPromise();
+                console.log('Sending order to /api/mercadopago/pay', order, 'type', this.paymentType);
+                const response = await nsHttpClient.post('/api/mercadopago/pay', { order, payment_type: this.paymentType }).toPromise();
                 console.log('MercadoPago pay response', response);
                 if (response.status === 'created' || response.status === 'paid') {
                     this.$emit('submit');
