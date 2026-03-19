adaed00d42be2be61bc62f2ac8b7af3d07cc1c4b
render4us <vitormartinsg@hotmail.com>
2026-03-19 11:10:46 -0300

    Update HomeController.php

--- app/Http/Controllers/Dashboard/HomeController.php
+++ app/Http/Controllers/Dashboard/HomeController.php
@@ -9,17 +9,11 @@
 namespace App\Http\Controllers\Dashboard;
 
 use App\Http\Controllers\DashboardController;
-use Illuminate\Support\Facades\View;
 
 class HomeController extends DashboardController
 {
     public function welcome()
     {
-        return View::make( 'welcome', [
-            'title' => sprintf(
-                __( 'Welcome &mdash; %s' ),
-                ns()->option->get( 'ns_store_name', 'SnowSYS ' . config( 'nexopos.version' ) )
-            ),
-        ] );
+        return redirect( '/sign-in' );
     }
 }
